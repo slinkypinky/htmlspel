66e3e71ece7545fd078000b08a57ab9e9abc9920
slinkypinky <slinkypinky@users.noreply.github.com>
2021-03-08 10:01:12 +0100

    added javascript

--- javascript.js
+++ javascript.js
@@ -1,81 +1,57 @@
 "use strict";
 
+const LOCAL_STORAGE_KEY_TODOS = "app.todos.advanced";
 
+let todos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_TODOS)) || [];
 
-/* 
-let todos = JSON.parse(localStorage.getItem("app.message-history")) || [" a"];
 let listRoot = document.querySelector("#list");
 let listForm = document.querySelector("[form]");
-let listInput = document.querySelector("[input]"); 
+let listInput = document.querySelector("[input]");
 
 listForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (listInput.value.trim() === "") {
-        return;
-    }
-    todos.push(listInput.value.trim());
-    updateList();
-    listInput.value = "";
+  e.preventDefault();
+  if (listInput.value.trim() === "") {
+    return;
+  }
+  todos.push(createTodo(listInput.value.trim()));
+  updateList();
+  listInput.value = "";
 });
 
-function todoList(items) {
-    let list = document.createElement("ul");
-    items.forEach((item) => {
-        let todoListItem = document.createElement("li");
+function createTodo(name) {
+  return {
+    id: Date.now().toString(),
+    name: name,
+  };
+}
 
-     /*   todoListItem.innerHTML = "<em>" + item.date + "</em> : <br>"  + item.message + "</br>";
-        item.reaction.forEach((reaction) => {
-            todoListItem.innerHTML += reaction;
-        }); */ /*
-        todoListItem.classList.add("thingtodo");
-        todoListItem.addEventListener("click", removeItem);
-        list.append(todoListItem);
-    });
-    return list;
+function todoList(items) {
+  let list = document.createElement("ul");
+  items.forEach((item) => {
+    let todoListItem = document.createElement("li");
+    todoListItem.innerText = item.name;
+    todoListItem.setAttribute("data-id", item.id);
+    todoListItem.classList.add("todo-list-item");
+    todoListItem.addEventListener("click", removeItem);
+    list.append(todoListItem);
+  });
+  return list;
 }
 
 function removeItem(event) {
-    let itemToRemove = event.target.innerText;
-    todos = todos.filter((item) => item !== itemToRemove);
-    updateList();
+  let itemToRemove = event.target.getAttribute("data-id");
+  todos = todos.filter((item) => item.id !== itemToRemove);
+  updateList();
 }
 
 function updateList() {
-    saveHistory();
-    let likeButton = document.createElement("button");
-        likeButton.innerText = "Love!";
-        likeButton.setAttribute("data-message-id", item.id);
-        likeButton.addEventListener("click", (event) => {
-          let targetMessage =  items.find((value) => 
-              value.id === event.target.GetAttribute("data-message-id"));
-              targetMessage.reactions.push(":)");
-              updateList();
-        });
-        todoListItem.append(likeButton);
-    listRoot.innerHTML = "";
-    listRoot.append(todoList(todos));
+  saveList();
+  listRoot.innerHTML = "";
+  listRoot.append(todoList(todos));
 }
 
-function makeObject(message){
-    let messageObject ={
-    id: Date().now().toString(),
-    message: message,
-    date: new Date().toDateString(),
-    reactions: [],
- };
- return messageObject;
+function saveList() {
+  localStorage.setItem(LOCAL_STORAGE_KEY_TODOS, JSON.stringify(todos));
 }
-
-function saveHistory() {
-    localStorage.setItem("app.message-history", JSON.stringify(messageHistory));
-}
-
-function clearHistory(){
- /* localStorage.clearHistory(???) och messageHistory = todos, arrayen */
-} /*
 
 updateList();
-
-*/
-
-
